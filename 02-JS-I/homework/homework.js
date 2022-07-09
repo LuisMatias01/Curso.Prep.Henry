c8d79b257e527e8c8344d186b3a66aac78d9021c
luis Matias <luismatiasortiz58@gmail.com>
2022-07-09 10:25:10 -0500

    finish homwork 1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -174,7 +174,8 @@ function redondearHaciaArriba(num) {
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  
+  return Math.random();
+
 }
 
 function esPositivo(numero) {
@@ -182,26 +183,41 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
-}
+  if(numero === 0){
+    return false;
+     
+  }
+  else   if(numero>0){
+
+    return "el numero es positivo";
+  }
+  else{
+    return "el numero es negativo";
+  }
+
+  }
+
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  return str + '!';
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  var combinado = nombre +''+apellido;
+  return combinado; 
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+  return 'hola'+nombre+'!';
   
 }
 
